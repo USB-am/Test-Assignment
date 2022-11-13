cccc2db4caebe84fb89072efcb1031cdfe055956
USB-am <smit.acer@mail.ru>
2022-11-14 01:13:14 +0300

    Added Commitet block to home_page

--- home_page/views.py
+++ home_page/views.py
@@ -6,3 +6,9 @@ class HomePageView(TemplateView):
 	''' Представление главной страницы '''
 
 	template_name = 'home_page/home_page.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['commitet'] = [i for i in range(8)]
+		return context
