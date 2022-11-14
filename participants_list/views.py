a8db4dd5f9d274904562d2e1996b5fa0a2ed9c54
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-11-14 16:29:08 +0300

    Added Paginator to participants_list page

--- participants_list/views.py
+++ participants_list/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from registration.models import Participant
+
 
 class ParticipantsListView(ListView):
 	''' Представление списка зарегестрированных участников '''
+	model = Participant
 	template_name = 'participants_list/participants_list.html'
-	# Определить модель 
+	context_object_name = 'participants'
+	paginate_by = 10
